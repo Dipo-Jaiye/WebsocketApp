4f6fc98d2f2f640eddf5ad5befb5c3994b5ac5f6
apple <oladipupo@errandpay.com>
2025-01-11 21:17:48 +0100

    feat:enable new tabs spawn new users

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -32,7 +32,7 @@ export default {
     },
   },
   created() {
-    const sessionID = localStorage.getItem("sessionID");
+    const sessionID = sessionStorage.getItem("sessionID");
 
     if (sessionID) {
       this.usernameAlreadySelected = true;
@@ -43,8 +43,8 @@ export default {
     socket.on("session", ({ sessionID, userID }) => {
       // attach the session ID to the next reconnection attempts
       socket.auth = { sessionID };
-      // store it in the localStorage
-      localStorage.setItem("sessionID", sessionID);
+      // store it in the sessionStorage
+      sessionStorage.setItem("sessionID", sessionID);
       // save the ID of the user
       socket.userID = userID;
     });
